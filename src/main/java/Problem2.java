7ca8aae54bef4c11d0206de5bba34f9ebdf72606
Haaris Nadeem <haaris2001@gmail.com>
2020-10-12 15:38:26 -0700

    problem 2 description complete

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,7 +3,11 @@ import java.util.Arrays;
 public class Problem2 {
 
     /*
-    Describe your algorithm here in up to 5 sentences:
+    Because the array in input as sorted, if there is a duplicate number it will be right next to the first number.
+    If a duplicate is detected right next to if it will move the array from the right of the duplicate down to the
+    beginning of the array by one. It will continue to do that until it reaches the largest number where it will instead
+    break the loop. It will then read the array for the first largest number that will appear and take the length of the
+    array from the first largest number and add one to the index of that element to find the length.
      */
 
     // Do not change signature (function name, parameters and return type)
